e94789b65a2aefdf29358d375f48462cf614aecb
Yaroslav <yaroslavposkryakov@gmail.com>
2022-03-22 15:58:57 +0300

    test diff directories

---
+++ roll_out.py
@@ -0,0 +1,12 @@
+import sys
+
+if __name__ == "__main__":
+    n = len(sys.argv)
+    print("Total arguments passed:", n)
+
+    # Arguments passed
+    print("\nName of Python script:", sys.argv[0])
+
+    print("\nArguments passed:", end=" ")
+    for i in range(1, n):
+        print(sys.argv[i], end=" ")
