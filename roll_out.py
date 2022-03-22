9adfa4f83aadb008740103d5450a54c23255dcfb
Yaroslav <yaroslavposkryakov@gmail.com>
2022-03-22 21:28:24 +0300

    Fix roll_out.py

--- roll_out.py
+++ roll_out.py
@@ -1,65 +1,66 @@
 import psycopg2
 import os
+import logging
+logging.basicConfig(level=logging.INFO)
 
 
 def roll_out_ddl(file_path: str):
     splited_path = file_path.split("/")
     schema, table = splited_path[-2], splited_path[-1].split(".")[0]
-    host = os.environ['PGHOST']
-    port = os.environ['PGPORT']
-    database = os.environ['PGDATABASE']
-    user = os.environ['PGUSER']
-    password = os.environ['PGPASSWORD']
-    # host = "localhost"
-    # port = 5432
-    # database = "postgres"
-    # user = "postgres"
-    # password = "docker"
-    # schema = "public"
+    # host = os.environ['PGHOST']
+    # port = os.environ['PGPORT']
+    # database = os.environ['PGDATABASE']
+    # user = os.environ['PGUSER']
+    # password = os.environ['PGPASSWORD']
+    host = "localhost"
+    port = 5432
+    database = "postgres"
+    user = "postgres"
+    password = "docker"
+    schema = "public"
     with open(file_path) as f:
         query_creating = f.read()
     query_existed = f"""
         select exists (
-        select from 
-            pg_tables
-        where 
-            schemaname = '{schema}' and 
-            tablename  = '{table}'
+            select 
+              from pg_tables
+             where schemaname = '{schema}'
+               and tablename  = '{table}'
         )
         """
     query_empty = f"""
-        select count(1) from {schema}.{table} limit 1
+        select count(1)
+          from {schema}.{table}
+         limit 1
         """
     query_drop = f"""
         drop table {schema}.{table}
         """
     with psycopg2.connect(user=user, password=password, host=host, port=port, database=database) as connection:
-        print("PostgreSQL connection is opened\n")
         with connection.cursor() as cursor:
-            print("PostgreSQL cursor is opened\n")
-            print(f"Check is table already exists.\n{query_existed}\n")
+            logging.info(f"\nCheck is table already exists.\n{query_existed}\n")
             cursor.execute(query_existed)
             is_exists = cursor.fetchone()[0]
             is_empty = True
             if is_exists:
-                print(f"Check is table empty.\n{query_empty}\n")
+                logging.info(f"\nCheck is table empty.\n{query_empty}\n")
                 cursor.execute(query_empty)
                 is_empty = not bool(int(cursor.fetchone()[0]))
 
             if not is_exists:
-                print(f"Create table:\n{query_creating}\n")
+                logging.info(f"\nCreate table:\n{query_creating}\n")
                 cursor.execute(query_creating)
                 connection.commit()
-                print("Table was created.\n")
+                logging.info("\nTable was created.\n")
             elif is_exists and is_empty:
-                print(f"Drop table:\n{query_drop}\n")
+                logging.info(f"\nDrop table:\n{query_drop}\n")
                 cursor.execute(query_drop)
-                print(f"Recreate table:\n{query_creating}\n")
+                logging.info(f"\nRecreate table:\n{query_creating}\n")
                 cursor.execute(query_creating)
                 connection.commit()
-                print("Table was recreated.\n")
+                logging.info("\nTable was recreated.\n")
             else:
-                print("Table is exists and not empty.\n")
+                logging.info("\nTable is exists and not empty.\n")
 
 
 if __name__ == "__main__":
