1d8332b82205dfbafb28d160fc9a3fa541c8ce29
Yaroslav <yaroslavposkryakov@gmail.com>
2022-03-22 16:46:17 +0300

    test diff directories

--- roll_out.py
+++ roll_out.py
@@ -1,10 +1,39 @@
 import psycopg2
+from psycopg2 import Error
 import sys
 
 if __name__ == "__main__":
-    n = len(sys.argv)
-    print("Total arguments passed:", n)
-    print("\nArguments passed:", end=" ")
-    for i in range(1, n):
-        print(sys.argv[i], end=" ")
-    print("\n")
+    file_path = sys.argv[1]
+    host = sys.argv[2]
+    port = sys.argv[3]
+    database = sys.argv[4]
+    user = sys.argv[5]
+    password = sys.argv[6]
+    with open(file_path) as f:
+        create_query = f.read()
+    print(create_query)
+    try:
+        connection = psycopg2.connect(user=user,
+                                      password=password,
+                                      host=host,
+                                      port=port,
+                                      database=database)
+
+        cursor = connection.cursor()
+        # SQL query to create a new table
+        create_table_query = '''CREATE TABLE mobile
+              (ID INT PRIMARY KEY     NOT NULL,
+              MODEL           TEXT    NOT NULL,
+              PRICE         REAL); '''
+        # Execute a command: this creates a new table
+        cursor.execute(create_table_query)
+        connection.commit()
+        print("Table created successfully in PostgreSQL ")
+
+    except (Exception, Error) as error:
+        print("Error while connecting to PostgreSQL", error)
+    finally:
+        if connection:
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
